1981d0baf1b31731c94dbcb10b08bb6583e25d5c
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-02-19 17:00:45 -0500

    Update Memory.h

--- Source/SolidMacros/Memory/Memory.h
+++ Source/SolidMacros/Memory/Memory.h
@@ -8,6 +8,17 @@
 
 namespace Solid::Memory
 {
+	template <typename T>
+	FORCEINLINE CONSTEXPR NO_DISCARD uint32 GetAlignment()
+	{
+		if CONSTEXPR (!std::is_empty_v<T>)
+		{
+			return std::min(sizeof(uintptr_t), alignof(T));
+		}
+		
+		return 0;
+	}
+	
 	/**
 	 * Returns the address of the object at the given offset from the given pointer.
 	 * 
@@ -49,6 +60,91 @@ namespace Solid::Memory
 		{
 			FPlatformMisc::Prefetch(Address, LineIdx * PLATFORM_CACHE_LINE_SIZE);
 		}
+	}
+
+	template <typename T, typename V>
+	FORCEINLINE CONSTEXPR T Align(T Num, V Alignment)
+	{
+		return Alignment == 0 ? Num : ((Num + (Alignment - 1)) / Alignment) * Alignment;
+	}
+
+	template <uint64 Alignment, typename T>
+	FORCEINLINE CONSTEXPR T Align(T Num)
+	{
+		return (Num + (Alignment - 1)) & ~(Alignment - 1);
+	}
+
+	template <typename T, typename V>
+	FORCEINLINE CONSTEXPR NO_DISCARD uint32 Padding(T Num, V Alignment)
+	{
+		return static_cast<uint32>(Align(Num, Alignment) - Num);
+	}
+
+	template <uint64 Alignment, typename T>
+	FORCEINLINE CONSTEXPR NO_DISCARD uint32 Padding(T Num)
+	{
+		return static_cast<uint32>(Align<Alignment>(Num) - Num);
+	}
+
+	template <typename DestinationType, typename SourceType>
+	FORCEINLINE CONSTEXPR NO_DISCARD DestinationType BitCast(const SourceType& Source)
+	{
+		static_assert(sizeof(DestinationType) == sizeof(SourceType),
+			"BitCast requires the types to have the same size.");
+		
+		static_assert(std::is_trivially_copyable_v<DestinationType>,
+			"BitCast requires the destination type to be trivially copyable.");
+		
+		static_assert(std::is_trivially_copyable_v<SourceType>,
+			"BitCast requires the source type to be trivially copyable.");
+		
+		DestinationType Destination;
+		FMemory::Memmove(static_cast<void*>(&Destination), static_cast<const void*>(&Source), sizeof(DestinationType));
+		return Destination;
+	}
+
+	template <typename T>
+	struct TUnalignedReference
+	{
+		using Type = T;
+		
+	public:
+		FORCEINLINE TUnalignedReference(void* InPtr)
+			: Ptr(InPtr)
+		{
+		}
+
+		FORCEINLINE TUnalignedReference& operator=(const T& Value)
+		{
+			FMemory::Memmove(Ptr, static_cast<const void*>(&Value), sizeof(T));
+			return *this;
+		}
+
+		FORCEINLINE operator T() const
+		{
+			T Value;
+			FMemory::Memmove(static_cast<void*>(&Value), Ptr, sizeof(T));
+			return Value;
+		}
+
+	private:
+		void* Ptr;
+	}; // struct TUnalignedReference
+
+	FORCEINLINE CONSTEXPR NO_DISCARD uint64 GetAlignedByteOffset(uintptr_t Ptr,
+		const uint64 ElementAlignment, const uint64 ElementSize, const uint64 Count)
+	{
+		const uint64 Padding = Memory::Padding(Ptr, ElementAlignment);
+		Ptr += Padding + ElementSize * Count;
+		return Ptr;
+	}
+
+	template <typename T, uint64 Alignment>
+	FORCEINLINE CONSTEXPR NO_DISCARD uint64 GetAlignedByteOffset(uintptr_t Ptr, const uint64 Count)
+	{
+		const uint64 Padding = Memory::Padding<Alignment>(Ptr);
+		Ptr += Padding + sizeof(T) * Count;
+		return Ptr;
 	}
 	
 } // namespace Solid::Memory
