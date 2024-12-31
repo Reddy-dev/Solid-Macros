6a0ff45e017b4fc833c81917550b8fcc95e78024
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2024-12-31 14:12:34 -0500

    Moved Message Log

--- Source/SolidMacros/Logs/Unlog/Unlog.h
+++ Source/SolidMacros/Logs/Unlog/Unlog.h
@@ -8,6 +8,10 @@
 #include <Templates/EnableIf.h>
 #include <Templates/IsArrayOrRefOfType.h>
 
+#include <Developer/MessageLog/Public/MessageLogModule.h>
+#include <Developer/MessageLog/Public/IMessageLogListing.h>
+#include <Modules/ModuleManager.h>
+
 #define UNLOG_VERSION TEXT("0.5")
 #define UNLOG_ENABLED (!UE_BUILD_SHIPPING)
 #define UNLOG_COMPILED_OUT  
@@ -896,4 +900,50 @@ namespace UnlogMacroHelpers
 
 #endif // UNLOG_ENABLED
 
-using Unlog = TUnlog<>;
+namespace Target
+{
+    struct MessageLog
+    {
+        static TSharedRef<IMessageLogListing> GetLogListing(FMessageLogModule& MessageLogModule, const FName& CategoryName)
+        {
+            TSharedRef<IMessageLogListing> Listing = MessageLogModule.GetLogListing(CategoryName);
+            Listing->SetLabel(FText::FromString(CategoryName.ToString()));
+            return Listing;
+        }
+
+        static EMessageSeverity::Type VerbosityToSeverity(const ELogVerbosity::Type Verbosity)
+        {
+            switch (Verbosity)
+            {
+                case ELogVerbosity::Error:
+                    return EMessageSeverity::Error;
+                case ELogVerbosity::Warning:
+                    return EMessageSeverity::Warning;
+                default: ;
+            }
+            return EMessageSeverity::Info;
+        }
+
+        static void Call(const UnlogCategoryBase& Category, const ELogVerbosity::Type Verbosity, const FString& Message)
+        {
+            FMessageLogModule& MessageLogModule = FModuleManager::LoadModuleChecked<FMessageLogModule>("MessageLog");
+
+            const TSharedRef<IMessageLogListing> LogListing = GetLogListing(MessageLogModule, Category.GetName());
+            LogListing->AddMessage(
+                FTokenizedMessage::Create(
+                    VerbosityToSeverity(Verbosity),
+                    FText::FromString(Message)
+                )
+            );
+
+            if (Verbosity == ELogVerbosity::Error)
+            {
+                MessageLogModule.OpenMessageLog(Category.GetName());
+            }
+        }
+        
+    }; // struct MessageLog
+    
+} // namespace Target
+
+using Unlog = TUnlog<Target::UELog>;
