38c33d934fcd45a65be1b855b6c21258ece2456e
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-06-15 17:01:47 -0400

    Engine Update and Logging Update

---
+++ Source/SolidMacrosTests/SolidMacrosTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "SolidMacrosTests.h"
+
+#define LOCTEXT_NAMESPACE "FSolidMacrosTestsModule"
+
+void FSolidMacrosTestsModule::StartupModule()
+{
+    
+}
+
+void FSolidMacrosTestsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSolidMacrosTestsModule, SolidMacrosTests)
